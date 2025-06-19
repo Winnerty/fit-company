eaa4e2757d0a089213a6c1c935b702d663aa0b4c
Str4ch <dog.fek54@gmail.com>
2025-06-19 10:56:45 +0200

    basic billing

---
+++ src/billing/models_db.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Date
+from .database import Base
+
+class BillingInfoModel(Base):
+    __tablename__ = "billing_info"
+
+    id = Column(Integer, primary_key=True, index=True)
+    #user_id = Column(Integer, nullable=False)
+    credit_card_number = Column(String(16), nullable=False)
+    expiration_date = Column(Date, nullable=False)
+    cvv = Column(String(3), nullable=False)
+    card_holder_name = Column(String(100), nullable=False)
+    email = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<BillingInfo(id={self.id}, card_holder_name='{self.card_holder_name}')>"
